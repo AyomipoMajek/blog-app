da3bb34ef9b9d96deb614890a9a8ef28114958fc
Ayomipo Majek <ayomipomajek@gmail.com>
2023-06-19 18:01:14 +0100

    controller setup implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  root 'pages#blog'
+  root "users#index"
+  # root 'pages#blog'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create]
+  end
+
+  resources :posts do
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
