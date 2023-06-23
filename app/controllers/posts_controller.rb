5c5ce11d37b5bde43e43fc69dc59cc94693f7821
Ayomipo Majek <ayomipomajek@gmail.com>
2023-06-23 22:52:21 +0100

    forms implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,28 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
+    if @post.save
+      flash[:success] = 'Post has been created successfully!'
+      redirect_to user_posts_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
