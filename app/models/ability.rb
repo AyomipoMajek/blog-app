e8531c1b360f8b2fed2b2fc643d7278df66af367
Ayomipo Majek <ayomipomajek@gmail.com>
2023-06-29 20:23:53 +0100

    authorization implemented

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # Guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :destroy, Post, user_id: user.id
+      can :destroy, Comment, user_id: user.id
+    end
+  end
+end
